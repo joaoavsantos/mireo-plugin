11c161e09897bf70915b38cdc4e59e8c26147b56
joaoavsantos <42188271+joaoavsantos@users.noreply.github.com>
2021-09-23 15:08:44 +0100

    Create MireoPlugin.java

---
+++ src/android/com/teamresilience/mireo/plugin/MireoPlugin.java
@@ -0,0 +1,141 @@
+package com.teamresilience.mireo.plugin;
+
+import android.util.Log;
+import android.widget.Toast;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import hr.mireo.arthur.api.API;
+import hr.mireo.arthur.api.APIAsyncRequest;
+import hr.mireo.arthur.api.EasyAPI;
+import hr.mireo.arthur.api.GeoAddress;
+
+public class MireoPlugin extends CordovaPlugin {
+
+    private static final String TAG = "MireoPlugin";
+
+    private EasyAPI mAPI;
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+
+        Log.d(TAG, "Initializing Mireo Cordova plugin");
+
+        // create API instance
+        mAPI =  new EasyAPI("gm", cordova.getContext());
+
+
+        // GeoAddress address = new GeoAddress();
+        // address.area = "Overijssel|OV";
+        // address.houseNumber = String.valueOf(53);
+        // address.postal = "7711";
+        // address.country = "Nederland";
+        // address.iso = 528;
+        // address.street = "Bosmansweg";
+        // address.city = "Nieuwleusen, Dalfsen";
+        // address.setLonLat(6.276339590549469, 52.58167967539245);
+        // address.type = String.valueOf(1080);
+
+        // EasyAPI.AddressResult listener2 = (status, address1) -> {
+        //     String message = "onAddress - status = " + status + ", address = " + address1;
+
+        //     Toast toast = Toast.makeText(cordova.getActivity(), message,
+        //             Toast.LENGTH_LONG);
+        //     // Display toast
+        //     toast.show();
+        // };
+        // APIAsyncRequest apiAsyncRequest = mAPI.navigateTo(address, false, listener2);
+    }
+
+    public String getMireoVersion()
+    {
+        AtomicReference<String> result = new AtomicReference<>("Unknown");
+
+        mAPI.getVersionEx((status, version) -> {
+            if (status == API.RESULT_OK) {
+                Log.d(TAG, "Mireo Application version: " + version.appVersion);
+                Log.d(TAG, "Mireo Build number: " + version.buildNumber);
+                Log.d(TAG, "Mireo Map version: " + version.mapVersion);
+                Log.d(TAG, "Mireo Device serial number: " + version.serial);
+
+                result.set(version.formatAppVersion(false));
+            }
+            else {
+                Log.e(TAG, "getVersionEx ERROR: " + EasyAPI.statusMessage(status));
+                result.set(EasyAPI.statusMessage(status));
+            }
+        }).waitForResult(20_000);
+
+        return result.get();
+    }
+
+    public boolean navigateTo(String street, Integer houseNumber, Integer postal, String city, String area, String country, Integer countryIso, Double lon, Double lat, boolean noUI)
+    {
+        AtomicReference<Integer> apiResult = new AtomicReference<>(API.RESULT_FAIL);
+
+        GeoAddress address = new GeoAddress();
+        address.area = area; //"Overijssel|OV";
+        address.houseNumber = String.valueOf(houseNumber); //String.valueOf(53);
+        address.postal = String.valueOf(postal); //"7711";
+        address.country = country; // "Nederland";
+        address.iso = countryIso; //528;
+        address.street = street; //"Bosmansweg";
+        address.city = city; //"Nieuwleusen, Dalfsen";
+//        address.setLonLat(6.276339590549469, 52.58167967539245);
+        address.setLonLat(lon, lat);
+//        address.type = String.valueOf(1080);
+
+        EasyAPI.AddressResult listener = (status, foundAddress) -> {
+            apiResult.set(status);
+        };
+        mAPI.navigateTo(address, noUI, listener).waitForResult(20_000);
+
+        return apiResult.get() == API.RESULT_OK;
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray args,
+                           final CallbackContext callbackContext) throws JSONException {
+        if("getVersion".equals(action))
+        {
+            String mireoVersion = getMireoVersion();
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, mireoVersion);
+            callbackContext.sendPluginResult(pluginResult);
+            return true;
+        }
+        else if("navigateTo".equals(action))
+        {
+            JSONObject options = args.getJSONObject(0);
+
+            String street = options.optString("street");
+            Integer houseNumber = options.optInt("houseNumber", -1);
+            Integer postal = options.optInt("postal", -1);
+            String city = options.optString("city");
+            String area = options.optString("area");
+            String country = options.optString("country");
+            Integer countryIso = options.optInt("countryIso", -1);
+            Double lon = options.optDouble("lon", Double.NaN);
+            Double lat = options.optDouble("lat", Double.NaN);
+            boolean noUI = options.getBoolean("noUI");
+
+            boolean navigationResult = navigateTo(street, houseNumber, postal, city, area, country, countryIso, lon, lat, noUI);
+
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, navigationResult);
+            callbackContext.sendPluginResult(pluginResult);
+            return true;
+        }
+        else
+        {
+            callbackContext.error("\"" + action + "\" is not a recognized action.");
+            return false;
+        }
+    }
+}
